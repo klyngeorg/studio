04ce6d64ffe82b684cf87b5c171e6efffcb95bd4
Simen A. W. Olsen <so@bjerk.io>
2023-09-11 20:44:02 +0200

    add groqd playground and devmode

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,19 @@
 import { visionTool } from '@sanity/vision';
+import { groqdPlaygroundTool } from 'groqd-playground';
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { defaultDesk } from './desks/default.js';
 import { schemaTypes } from './schemas';
 
+function isDevMode() {
+  const devInSearch = window ? window.location.search.includes('dev') : false;
+  const devInLocalStorage = localStorage ? localStorage.getItem('devMode') === 'true' : false;
+  const devModeEnabled = devInLocalStorage || devInSearch;
+  return devModeEnabled;
+}
+
 const isDevelopment = process.env.NODE_ENV === 'development';
+const showDevTools = isDevelopment || isDevMode();
 
 const devPlugins = [
   deskTool({
@@ -12,6 +21,7 @@ const devPlugins = [
     name: 'admin-desk',
   }),
   visionTool(),
+  groqdPlaygroundTool(),
 ];
 
 export default defineConfig({
@@ -21,7 +31,7 @@ export default defineConfig({
   projectId: '95rc74zt',
   dataset: 'production',
 
-  plugins: [defaultDesk, ...(isDevelopment ? devPlugins : [])],
+  plugins: [defaultDesk, ...(showDevTools ? devPlugins : [])],
 
   schema: {
     types: schemaTypes,
